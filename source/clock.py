3787ae402437e8095efb1e5e73334617ba34a73f
AbdirayimovS <abdirayimovsardor20@gmail.com>
2025-02-16 15:08:39 +0900

    [Fix] Add QGradient, and clean up paintEvent

--- source/clock.py
+++ source/clock.py
@@ -6,7 +6,7 @@ import math
 
 from PyQt5.QtWidgets import QWidget
 from PyQt5.QtCore import Qt, QTimer
-from PyQt5.QtGui import QColor, QPainter, QPen
+from PyQt5.QtGui import QColor, QPainter, QPen, QBrush, QGradient, QLinearGradient, QRadialGradient, QConicalGradient
 
 
 class Clock(QWidget):
@@ -18,6 +18,7 @@ class Clock(QWidget):
 
     https://www.geeksforgeeks.org/create-analog-clock-using-pyqt5-in-python/
     '''
+
     def __init__(self, hour=10, minute=30, parent=None):
         super().__init__(parent)
         self.__minute = minute
@@ -25,6 +26,24 @@ class Clock(QWidget):
         self._previous_minute = minute
         self._previous_hour = hour
         self.percent = 0
+        self.HOUR_HAND_PEN = QPen(Qt.white,
+                                    10,
+                                    Qt.SolidLine,
+                                    Qt.RoundCap,
+                                    Qt.RoundJoin
+                                    )
+        self.MINUTE_HAND_PEN = QPen(Qt.white,
+                                    10,
+                                    Qt.SolidLine,
+                                    Qt.RoundCap,
+                                    Qt.RoundJoin
+                                    )
+        self.SECOND_HAND_PEN = QPen(Qt.red,
+                                    5,
+                                    Qt.SolidLine,
+                                    Qt.RoundCap,
+                                    Qt.RoundJoin
+                                    )
         self.delta_second = 0
         self.second_degree = self.delta_second * 6.
         self.minute_degree = 0.
@@ -33,9 +52,14 @@ class Clock(QWidget):
         self.center_x = self.rec // 2
         self.center_y =  self.rec // 2
         self.radius = self.rec - self.center_x
+
+        self.hours_line_len = self.radius * 0.6
+        self.minute_line_len = self.radius * 0.9
+        self.second_line_len = self.radius * 0.95
+
         timer = QTimer(self)
         timer.timeout.connect(self.updateHands)
-        timer.start(100)
+        timer.start(1000)
     
     def updateHands(self):
         # the hour hand is moved only if the minute hand is moved!
@@ -44,6 +68,15 @@ class Clock(QWidget):
         # if current time too far, then the second degree shall be increased, and later it shall be decreased to normal in the end of the reach!
         self.minute_degree += self.second_degree / 60 / 60
         self.hour_degree += self.minute_degree / 30 / 60 / 60
+
+        if int(self.second_degree) == 360:
+            self.second_degree = 0
+
+        if int(self.minute_degree) == 360:
+            self.minute_degree = 0
+        
+        if int(self.hour_degree) == 360:
+            self.hour_degree = 0
 
         # shall it be recursive and call itself to continuesly draw
         self.update()
@@ -90,55 +123,38 @@ class Clock(QWidget):
         painter.setRenderHint(QPainter.Antialiasing)
 
         #<<<<<<<Drawing a circle!>>>>>>>>
+        # Gradient: https://www.bogotobogo.com/Qt/Qt5_QLinear_QRadial_QConical_QGradient.php
+        brush = QBrush(QColor(0, 0, 255))  # create a gradient brush
 
-        painter.setPen(
-            QPen(
-                QColor(128, 128, 128),  # Color (gray)
-                5,
-                Qt.SolidLine,
-                Qt.RoundCap | Qt.MiterJoin,
-                )
-            )
+        # linear gradient
+        # gradient = QLinearGradient(0, 0, 400, 0)
+        # gradient.setColorAt(0.0, QColor(0, 0, 255))  # blue at the top 
+        # gradient.setColorAt(1.0, QColor(60, 60, 180))  # dark blue at the 
+
+        # <<<<<QRadialGradient>>>>>>
+        gradient = QRadialGradient(self.center_x, self.center_y, self.center_x) 
+        gradient.setColorAt(0.0, QColor(0, 30, 255))  # blue at the top 
+        gradient.setColorAt(1.0, QColor(60, 60, 180))  # dark blue at the 
+  ##################################
+
+        #<<<<<<<<<QConuqualGradient>>>>>>>
+        # gradient = QConicalGradient(200, 50, 250)
+        # gradient.setColorAt(0.0, QColor(0, 0, 255))  # blue at the top 
+        # gradient.setColorAt(1.0, QColor(60, 60, 180))  # dark blue at the 
+
+        #############################
+        brush = QBrush(gradient)
+        painter.setBrush(brush)
         
         painter.drawEllipse(0, 0, self.rec, self.rec)
 
         ##################################
 
 
-        #######<DRAWING>######
-        painter.setPen(
-            QPen(Qt.black,
-                 5,
-                 Qt.SolidLine,
-                 Qt.RoundCap,
-                 Qt.RoundJoin
-                )
-            )
-        
+
+        #######<DRAWING>#####
         # clean up due to degree values are not cyclic
-        if int(self.second_degree) == 360:
-            self.second_degree = 0
-
-        if int(self.minute_degree) == 360:
-            self.minute_degree = 0
         
-        if int(self.hour_degree) == 360:
-            self.hour_degree = 0
-        
-        
-        # draw a seconds (optional)
-        # What is the independent value -> second_x and second_y
-        # minutes are derived from the second_x and second_y
-        # hours are derived form minutes and seconds
-        #<<<<<<<<<<<<<<TWO WAYS>>>>>>>>>>>>>>>
-        # shall we calculate here the decision?
-        # Shall we calculate in other place?
-        # In order to remain simplicity, lets calculate here for now.
-        print(self.second_degree, self.minute_degree, self.hour_degree)
-
-        hours_line_len = self.radius * 0.6
-        minute_line_len = self.radius * 0.9
-        second_line_len = self.radius * 0.95
 
         # why we multiple to 30? 360 / 30  = 12. In order to easily classify into twelve points?
         # Is it necessary to move to cos and sin?
@@ -149,30 +165,28 @@ class Clock(QWidget):
         # radian minute = math.radians(delta_minute *6 + radian_second)
         # 1 cycle is 360 degree
         # second_degree must be between [0, 6]. Where 0 is start time when second is 0 and 6 is second is 0 but one increment
-        
-        
-        
         radian_second = math.radians(self.second_degree)  # because the second has 360 values?
         radian_minute = math.radians(self.minute_degree)  # + second value influence. It has also in radians influence i bet
         radian_hour = math.radians(self.hour_degree) # + radian_minute_influence. Radian minute is composition of minute and second
-        second_x = int(second_line_len * math.sin(radian_second) + self.center_x)
-        second_y = int(-1 * second_line_len * math.cos(radian_second) + self.center_y)
+        second_x = int(self.second_line_len * math.sin(radian_second) + self.center_x)
+        second_y = int(-1 * self.second_line_len * math.cos(radian_second) + self.center_y)
 
-        minutes_x = int(minute_line_len * math.sin(radian_minute) + self.center_x)
-        minutes_y = int(-1 * minute_line_len * math.cos(radian_minute) + self.center_y)
-        hours_x = int(hours_line_len * math.sin(radian_hour) + self.center_x)
-        hours_y = int(-1 * hours_line_len * math.cos(radian_hour) + self.center_y)
+        minutes_x = int(self.minute_line_len * math.sin(radian_minute) + self.center_x)
+        minutes_y = int(-1 * self.minute_line_len * math.cos(radian_minute) + self.center_y)
+        hours_x = int(self.hours_line_len * math.sin(radian_hour) + self.center_x)
+        hours_y = int(-1 * self.hours_line_len * math.cos(radian_hour) + self.center_y)
 
+        painter.setPen(self.SECOND_HAND_PEN)
         painter.drawLine(
             self.center_x, self.center_y,
             second_x, second_y
             )
-
+        painter.setPen(self.MINUTE_HAND_PEN)
         painter.drawLine(
             self.center_x, self.center_y,
             minutes_x, minutes_y
             )
-
+        painter.setPen(self.HOUR_HAND_PEN)
         painter.drawLine(
             self.center_x, self.center_y,
             hours_x, hours_y
